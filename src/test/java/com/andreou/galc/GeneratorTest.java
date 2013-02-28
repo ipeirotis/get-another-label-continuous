1a39b8f4859b10be9d79f5fd427e27951eaaadab
Giorgos Andreou <allemaos@gmail.com>
2013-02-28 12:44:54 +0200

    separate data creation and execution tests

--- src/test/java/com/andreou/galc/GeneratorTest.java
+++ src/test/java/com/andreou/galc/GeneratorTest.java
@@ -34,12 +34,20 @@ public class GeneratorTest {
 	}
 	
 	@Test
+	public void testCreateNewJoinlyNormalData() throws Exception {
+		//executeOn("--input data/joinlynormal/assignedlabels.txt --evalObjects data/joinlynormal/evaluationObjects.txt --evalWorkers data/joinlynormal/evaluationWorkers.txt --correct data/joinlynormal/goldObjects.txt --synthetic data/joinlynormal/options.txt --output results/joinlynormal");
+	}
+	@Test
+	public void testCreateCurrentSyntheticData_verbose() throws Exception {
+		//executeOn("--input data/synthetic/assignedlabels.txt --evalObjects data/synthetic/evaluationObjects.txt --evalWorkers data/synthetic/evaluationWorkers.txt --correct data/synthetic/goldObjects.txt  --synthetic data/synthetic/synthetic-options.txt --output results/synthetic");
+	}
+	@Test
 	public void testNewJoinlyNormalData() throws Exception {
-		executeOn("--input data/joinlynormal/assignedlabels.txt --evalObjects data/joinlynormal/evaluationObjects.txt --evalWorkers data/joinlynormal/evaluationWorkers.txt --correct data/joinlynormal/goldObjects.txt --synthetic data/joinlynormal/options.txt --output results/joinlynormal");
+		executeOn("--input data/joinlynormal/assignedlabels.txt --evalObjects data/joinlynormal/evaluationObjects.txt --evalWorkers data/joinlynormal/evaluationWorkers.txt --correct data/joinlynormal/goldObjects.txt --output results/joinlynormal");
 	}
 	@Test
 	public void testCurrentSyntheticData_verbose() throws Exception {
-		executeOn("--input data/synthetic/assignedlabels.txt --evalObjects data/synthetic/evaluationObjects.txt --evalWorkers data/synthetic/evaluationWorkers.txt --correct data/synthetic/goldObjects.txt  --synthetic data/synthetic/synthetic-options.txt --output results/synthetic");
+		executeOn("--input data/synthetic/assignedlabels.txt --evalObjects data/synthetic/evaluationObjects.txt --evalWorkers data/synthetic/evaluationWorkers.txt --correct data/synthetic/goldObjects.txt --output results/synthetic");
 	}
 	@Test
 	public void testCurrentSyntheticData_labels() throws Exception {
